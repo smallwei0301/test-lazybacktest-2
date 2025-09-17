e0d2ce76269a45b922121ad85b868317f98dc1a6
smallwei0301 <smallwei0301@gmail.com>
2025-09-17 11:44:54 +0800

    Add files via upload

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,21 +1,60 @@
-
 const https = require('https');
 const url = require('url');
 
+// Helper to fetch with redirects
+const fetchWithRedirect = (requestUrl, options, retries = 3) => {
+    return new Promise((resolve, reject) => {
+        const request = https.get(requestUrl, options, (response) => {
+            if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location) {
+                if (retries > 0) {
+                    console.log(`Redirecting to: ${response.headers.location}`);
+                    fetchWithRedirect(response.headers.location, options, retries - 1)
+                        .then(resolve)
+                        .catch(reject);
+                } else {
+                    reject(new Error('Too many redirects'));
+                }
+            } else {
+                let body = '';
+                response.on('data', (chunk) => (body += chunk));
+                response.on('end', () => {
+                    resolve({
+                        statusCode: response.statusCode,
+                        headers: response.headers,
+                        body: body
+                    });
+                });
+            }
+        });
+        request.on('error', (err) => reject(err));
+        request.setTimeout(30000, () => {
+            request.destroy();
+            reject(new Error('Request timed out'));
+        });
+        request.end();
+    });
+};
+
 exports.handler = async function(event, context) {
     const { path, queryStringParameters } = event;
-    const apiPath = path.replace('/.netlify/functions/proxy', '').replace('/api', '');
+    const apiPath = path.replace('/api', ''); // e.g., /tpex/st43_result.php
 
     let targetUrl;
     const params = new url.URLSearchParams(queryStringParameters).toString();
 
     if (apiPath.startsWith('/tpex/')) {
-        const tpexPath = apiPath.replace('/tpex/', '');
-        targetUrl = `https://www.tpex.org.tw/web/stock/aftertrading/${tpexPath}?${params}`;
+        const tpexEndpoint = apiPath.replace('/tpex/', '');
+        if (tpexEndpoint === 'st43_result.php') {
+            targetUrl = `https://www.tpex.org.tw/web/stock/aftertrading/daily_trading_info/st43_result.php?${params}`;
+        } else if (tpexEndpoint === 'stk_quote_result.php') {
+            targetUrl = `https://www.tpex.org.tw/web/stock/aftertrading/daily_close_quotes/stk_quote_result.php?${params}`;
+        }
     } else if (apiPath.startsWith('/twse/')) {
-        const twsePath = apiPath.replace('/twse/', '');
-        targetUrl = `https://www.twse.com.tw/exchangeReport/${twsePath}?${params}`;
-    } else {
+        const twseEndpoint = apiPath.replace('/twse/', '');
+        targetUrl = `https://www.twse.com.tw/exchangeReport/${twseEndpoint}?${params}`;
+    }
+
+    if (!targetUrl) {
         return {
             statusCode: 404,
             body: JSON.stringify({ error: 'API path not found' })
@@ -23,35 +62,6 @@ exports.handler = async function(event, context) {
     }
 
     console.log(`Proxying to: ${targetUrl}`);
-
-    const fetchWithRedirect = (url, options, retries = 3) => {
-        return new Promise((resolve, reject) => {
-            const request = https.get(url, options, (response) => {
-                if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location) {
-                    if (retries > 0) {
-                        console.log(`Redirecting to: ${response.headers.location}`);
-                        fetchWithRedirect(response.headers.location, options, retries - 1)
-                            .then(resolve)
-                            .catch(reject);
-                    } else {
-                        reject(new Error('Too many redirects'));
-                    }
-                } else {
-                    let body = '';
-                    response.on('data', (chunk) => (body += chunk));
-                    response.on('end', () => {
-                        resolve({
-                            statusCode: response.statusCode,
-                            headers: response.headers,
-                            body: body
-                        });
-                    });
-                }
-            });
-            request.on('error', (err) => reject(err));
-            request.end();
-        });
-    };
 
     try {
         const response = await fetchWithRedirect(targetUrl, {
